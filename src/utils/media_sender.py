db0b6fa118b3196f1ea0fd6bf47dbccec2fb9f13
Frederik Lohner <freddylohner@gmail.com>
2016-02-10 12:13:18 -0700

    Boto-San is now scottish

--- src/utils/media_sender.py
+++ src/utils/media_sender.py
@@ -194,7 +194,7 @@ class EspeakTtsSender(AudioSender):
             logging.exception(e)
             self._on_error(jid)
 
-    def tts_record(self, text, lang='en-sc'):
+    def tts_record(self, text, lang='en-sc+m1'):
         file_path = self._build_file_path(text)
         cmd = "espeak -v%s -w %s -s 135 '%s'" % (lang, file_path, text)
         subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).wait()
