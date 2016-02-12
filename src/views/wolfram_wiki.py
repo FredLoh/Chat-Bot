fecdd28ec23b55716ba285adb39383a64eb7148e
Frederik Lohner <freddylohner@gmail.com>
2016-02-11 21:28:25 -0700

    Added wikipedia support

---
+++ src/views/wolfram_wiki.py
@@ -0,0 +1,23 @@
+"""
+    Wolfram Alpha:
+    /wa <term>
+    key = "KYPHH3-83LWW2GT5T"
+"""
+
+
+from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
+import unirest
+import wikipedia
+
+class WolframWiki():
+    def __init__(self, interface_layer):
+        self.interface_layer = interface_layer
+        self.routes = [
+            # ("^/wa", self.beban_spell_checker),
+            ("^/wi(kipedia)?\s(?P<query>[^$]+)$", self.wikipedia_query)
+        ]
+
+    def wikipedia_query(self, message, match):
+        query = match.group("query")
+        result = wikipedia.summary(query, sentences=1)
+        return TextMessageProtocolEntity(result, to=message.getFrom())
